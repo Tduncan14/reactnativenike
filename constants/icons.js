1d5b065d551214a19d89e7ebc4af6d7702af1fc6
tduncan14 <nbatre1@yahoo.com>
2022-04-08 17:15:56 -0700

    keep moving forward

---
+++ constants/icons.js
@@ -0,0 +1,2 @@
+export const menu = require('../assets/icons/menu.png');
+export const search = require('../assets/icons/search.png');
---
+++ constants/images.js
@@ -0,0 +1,13 @@
+export const nikeMetcon5Black = require('../assets/images/nike-metcon-5-black.png');
+export const nikeMetcon5purple = require ('../assets/images/nike-metcon-5-purple.png');
+export const nikeMetcon6 = require('../assets/images/nike-metcon-6.png');
+export const nikeMetconFree = require('../assets/images/nike-metcon-free.png');
+export const nikePegaus36 = require('../assets/images/nike-pegasus-36.png');
+export const nikeZoomKobe1Proto = require('../assets/images/nike-zoom-kobe-1-Proto.png');
+export const nikeMetcon3 = require('../assets/images/nike-metcon-4.png');
+export const recentlyViewedLabel = require('../assets/images/recently-viewed-label.png');
+
+
+
+
+
