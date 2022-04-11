ff239b2f5dd444a6c1cf0627d32febdf6791ef87
tduncan14 <nbatre1@yahoo.com>
2022-04-11 14:12:26 -0700

    keep moving forward

--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import {View,Text,StyleSheet,FlatList} from 'react-native';
+import {View,Text,StyleSheet,FlatList,TouchableOpacity,Image} from 'react-native';
 import { COLORS ,images,FONTS,SIZES} from '../constants';
 
 
@@ -44,22 +44,80 @@ const Home = () => {
 
 
 
+   const rendTrendingShoes = (item,index) => (
 
+
+    <TouchableOpacity
+       style={{height:240, width:180, justifyContent:'center',marginHorizontal:SIZES.base}}
+    
+    >
+        <Text style={{color:COLORS.gray, ...FONTS.h5}}>{item.type}</Text>
+
+
+        <View
+        style={{
+            flex:1,
+            justifyContent:'flex-end',
+            marginTop:SIZES.base,
+            borderRadius:10,
+            marginRight:SIZES.padding,
+            paddingLeft:SIZES.radius,
+            paddingRight:SIZES.padding,
+            paddingBottom:SIZES.radius,
+            backgroundColor:item.bgColor
+        }}>
+
+            <View style={{height:'35%',justifyContent:'space-between'}}>
+                <Text style={{color:COLORS.white, ...FONTS.body4}}>{item.name}</Text>
+                <Text style={{color:COLORS.white, ...FONTS.h3}}>{item.price}</Text>
+            </View>
+
+
+           <Image 
+           source={item.img}
+           resizeMode="cover"
+           style={{
+               position:'absolute',
+               top:50,
+               right:0,
+               width:'98%',
+               height:80,
+               transform:[
+                   {rotate:'-15'}
+               ]
+           }}
+           
+           
+           />
+        </View>
+    </TouchableOpacity>
+
+   )
 
 
 
 
     return (
         <View style={styles.container}>
-            <Text style={{marginTop:SIZES.radius, marginHorizontal:SIZES.padding, ...FONTS.large}}>TRENDING</Text>
+            <Text style={{marginTop:SIZES.radius, marginHorizontal:SIZES.padding, ...FONTS.large, color:'black'}}>TRENDING</Text>
 
             {/* {Trending} */}
 
             <View style={{height:260,marginTop:SIZES.radius}}>
 
-                <FlatList>
+                <FlatList
+                  horizontal
+                  showsHorizontalScrollIndicator={false}
+                  data={trending}
+                  keyExtractor={item => item.id.toString()}
+                  renderItem={({item,index})=> rendTrendingShoes(item,index)}
+                
+                
+                
+                
+                />
 
-                </FlatList>
+             
 
             </View>
         </View>
