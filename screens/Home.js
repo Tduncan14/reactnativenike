26df70bf20666400089b915cf1c1de5340ac0369
tduncan14 <nbatre1@yahoo.com>
2022-04-11 13:52:26 -0700

    keep moving forward

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,46 @@
-import {View,Text,StyleSheet} from 'react-native';
+import { useState } from 'react'
+import {View,Text,StyleSheet,FlatList} from 'react-native';
+import { COLORS ,images,FONTS,SIZES} from '../constants';
+
 
 
 
 
 const Home = () => {
+
+
+    // Dummy Data
+
+
+    const [trending,setTrending] = useState([
+        {
+            id: 0,
+            name: "Nike Air Zoom Pegasus 36",
+            img: images.nikePegasus36,
+            bgColor: "#BF012C",
+            type: "RUNNING",
+            price: "$186",
+            sizes: [6, 7, 8, 9, 10]
+        },
+        {
+            id: 1,
+            name: "Nike Metcon 5",
+            img: images.nikeMetcon5Black,
+            bgColor: "#D39C67",
+            type: "TRAINING",
+            price: "$135",
+            sizes: [6, 7, 8, 9, 10, 11, 12]
+        },
+        {
+            id: 2,
+            name: "Nike Air Zoom Kobe 1 Proto",
+            img: images.nikeZoomKobe1Proto,
+            bgColor: "#7052A0",
+            type: "BASKETBALL",
+            price: "$199",
+            sizes: [6, 7, 8, 9]
+        },
+]);
 
 
 
@@ -13,8 +50,18 @@ const Home = () => {
 
 
     return (
-        <View>
-            <Text style={styles.text}> Homee</Text>
+        <View style={styles.container}>
+            <Text style={{marginTop:SIZES.radius, marginHorizontal:SIZES.padding, ...FONTS.large}}>TRENDING</Text>
+
+            {/* {Trending} */}
+
+            <View style={{height:260,marginTop:SIZES.radius}}>
+
+                <FlatList>
+
+                </FlatList>
+
+            </View>
         </View>
     )
 }
@@ -23,8 +70,9 @@ const Home = () => {
 
 const styles = StyleSheet.create({
 
-    text:{
-        color:'black'
+    container:{
+        flex:1,
+        backgroundColor:COLORS.white
     }
 
 
